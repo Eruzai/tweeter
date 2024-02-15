9b0a16993e1dc7c1bb5040ca97d8851b9f73c8f1
Kevin Levasseur <kevin_levasseur@telus.net>
2024-02-15 14:15:30 -0800

    Counts characters and sets class if characters remaining falls below 0

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+// Character counter script for composing tweets
+$(document).ready(function() {
+  $("#tweet-text").on('input', function() {
+
+    const text = $(this).val()
+
+    const remainingChars = 140 - text.length;
+
+    const counter = $(this).parent().find('.counter');
+
+    counter.text(remainingChars);
+
+    remainingChars < 0 ? counter.addClass('char-overflow') : counter.removeClass('char-overflow');
+  })
+});
