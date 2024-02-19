b5565b355a88f4b6dfe4d1423d0e088713ba63ae
Kevin Levasseur <kevin_levasseur@telus.net>
2024-02-19 15:14:46 -0800

    hard coded data array, function loops through array and posts tweets to page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,8 +1,30 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+// Function creates and returns HTML article tag containing information from a tweet object
 $(document).ready(function() {
   const createTweetElement = function (tweetObject) {
     const profilePic = tweetObject.user.avatars;
@@ -33,24 +55,15 @@ $(document).ready(function() {
 </article>`);
 
     return $tweet;
-  }
-  
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
-  }
-  
-  const $tweet = createTweetElement(tweetData);
-  
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('.tweets').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  };
+
+  // Function loops through an array containing tweet objects and posts each of them to the page
+  const renderTweets = function (tweetObjectsArray) {
+    for (tweet of tweetObjectsArray) {
+      const $tweetToPost = createTweetElement(tweet);
+      $('.tweets-container').append($tweetToPost);
+    }
+  };
+
+  renderTweets(data);
 });
