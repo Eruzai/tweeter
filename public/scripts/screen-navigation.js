5b8d5a4038da25652fc89059160bb0c78690ac3e
Kevin Levasseur <kevin_levasseur@telus.net>
2024-02-21 17:21:25 -0800

    shows or hides buttons depending on page location

--- public/scripts/screen-navigation.js
+++ public/scripts/screen-navigation.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
   $(".window-scroll").on('click', function() {
-    $('html, body').animate({scrollTop: '400'}, 'slow', function() {$('#tweet-text').trigger('focus');})
+    $("html, body").animate({scrollTop: '400'}, 'slow', function() {
+      $('#tweet-text').trigger('focus');
+      $(".to-top").hide();
+      $("nav button").show();
+    });
+  });
+
+  $(window).on('scroll', function() {
+    const windowLocation = $(this).scrollTop();
+    if (windowLocation > 400) {
+      $(".to-top").show();
+      $("nav button").hide();
+    } else {
+      $(".to-top").hide();
+      $("nav button").show();
+    };
   });
 });
