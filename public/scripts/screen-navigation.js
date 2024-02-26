ffb61cc49375b2fe4b26d7de5cd009127280cf19
Kevin Levasseur <kevin_levasseur@telus.net>
2024-02-26 13:23:38 -0800

    gets offset of element to scroll too in different viewport styles

--- public/scripts/screen-navigation.js
+++ public/scripts/screen-navigation.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
   $(".window-scroll").on('click', function() {
-    $("html, body").animate({scrollTop: '400'}, 'slow', function() {
-      $('#tweet-text').trigger('focus');
+    const targetLocation = $("main").offset().top - 120;
+    $("html, body").animate({scrollTop: `${targetLocation}`}, 'slow', function() {
+      $("#tweet-text").trigger('focus');
       $(".to-top").hide();
       $("nav button").show();
     });
   });
-
+  
   $(window).on('scroll', function() {
     const windowLocation = $(this).scrollTop();
-    if (windowLocation > 400) {
+    const targetLocation = $("main").offset().top - 120;
+    if (windowLocation > targetLocation) {
       $(".to-top").show();
       $("nav button").hide();
     } else {
